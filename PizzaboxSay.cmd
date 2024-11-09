1695b09c9cd2bbb0315cadc24ec70a335284c6cc
Xalya <xalyabadinternet@outlook.com.gr>
2024-11-09 08:39:54 +0200

    !!LEAKERS STAY AWAY!!!
    i hope this dosen't get leaked.

---
+++ PizzaboxSay.cmd
@@ -0,0 +1,14 @@
+cscript voicecustom.vbs "Why are you angering me like this"
+cscript voicecustom.vbs "Do you even know what you're doing"
+cscript voicecustom.vbs "Why are you angering me like this"
+cscript voicecustom.vbs "Why are you angering me like this"
+cscript voicecustom.vbs "Why are you angering me like this"
+cscript voicecustom.vbs "Why are you angering me like this"
+cscript voicecustom.vbs "Why are you angering me like this"
+cscript voicecustom.vbs "Why are you angering me like this"
+cscript voicecustom.vbs "Why are you angering me like this"
+cscript voicecustom.vbs "Why are you angering me like this"
+title hello programer!
+pause
+cscript voicecustom.vbs "Hello programer!"
+exit
---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+this game was made by xalya.
+this game is NOT auctually a teatching game but something simmalar to baldis bascis
+i hope you enjoy this game i have been working on a while...
+check out my youtube channel https://www.youtube.com/@Xalya_insanity
+hope you dont anger pizzabox... because if you do... it will delete your game.
+do not harass pizzabox he is just your computer.
+there are a lot of hidden secrets i hope you can find them.
+and remember that ilikeeatingpizza.
